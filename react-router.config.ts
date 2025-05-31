8102376c84a4fcdaca9438389df7f7b4e56dc69d
Maksymilian Walicki <maxwalicki@gmail.com>
2025-05-31 21:29:35 +0200

    fx

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,4 +5,5 @@ export default {
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
   buildDirectory: "dist",
+  basename: "JordanAATravel",
 } satisfies Config;
