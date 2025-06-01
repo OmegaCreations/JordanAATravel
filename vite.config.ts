cc099ff0e524ea498ee5d11a135d52f7bf06315c
Maksymilian Walicki <maxwalicki@gmail.com>
2025-06-01 15:57:01 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: "/JordanAATravel/client", // Musi być zgodne z basename
+  base: "/JordanAATravel/client/", // Musi być zgodne z basename
   build: {
     outDir: "dist/",
     emptyOutDir: true,
