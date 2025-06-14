40d11c6bc888cc0edb7f7406738dc90d6e3999e5
Maksymilian Walicki <maxwalicki@gmail.com>
2025-06-14 16:29:09 +0200

    fix: change title and page description

--- app/root.tsx
+++ app/root.tsx
@@ -31,6 +31,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Jordan AA Trips - Explore Jordan with us</title>
+        <meta
+          name="description"
+          content="Discover tours, and experienced drivers in Jordan with Jordan AA Trips."
+        />
+
         <Meta />
         <Links />
       </head>
